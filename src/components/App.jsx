259050920994436d2f28ea0fff86d39713fb9a12
mkreynolds919 <mkreynolds919@gmail.com>
2025-07-17 15:06:16 -0500

    feat: fill Page component with skeleton subcomponents  and update App to use dynamic data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import '../styles/App.css'
+import Page from "./Page.jsx";
 import initialData from "../data/initialData.js";
 import { useState } from 'react';
 
 function App() {
-  const { header, summary, education, experience, skills, projects } = initialData;
   const [data, setData] = useState(initialData);
+  const { header, summary, education, experience, skills, projects } = data;
 
   return (
     <>
-      <Page data={data} setData={setData} header={header} summary={summary} education={education} experience={experience} skills={skills} projects={projects} />
+      <Page setData={setData} header={header} summary={summary} education={education} experience={experience} skills={skills} projects={projects} />
     </>
   )
 }
---
+++ src/components/Page.jsx
@@ -0,0 +1,14 @@
+
+export default function Page( { setData, header, summary, education, experience, skills, projects }) {
+
+    return (
+        <>
+            <Header data={header} setData={setData} />
+            <Summary data={summary} setData={setData} />
+            <Education data={education} setData={setData} />
+            <Experience data={experience} setData={setData} />
+            <Skills data={skills} setData={setData} />
+            <Projects data={projects} setData={setData} />
+        </>
+    );
+}
