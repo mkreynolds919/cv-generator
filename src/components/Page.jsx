81ce283449a5f68bec0fb9a5c9e02c6ed845a589
mkreynolds919 <mkreynolds919@gmail.com>
2025-07-18 12:13:34 -0500

    feat: wrap Page contents in div for containment and styling

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,13 +2,13 @@
 export default function Page( { setData, header, summary, education, experience, skills, projects }) {
 
     return (
-        <>
+        <div className="page-container">
             <Header data={header} setData={setData} />
             <Summary data={summary} setData={setData} />
             <Education data={education} setData={setData} />
             <Experience data={experience} setData={setData} />
             <Skills data={skills} setData={setData} />
             <Projects data={projects} setData={setData} />
-        </>
+        </div>
     );
 }
