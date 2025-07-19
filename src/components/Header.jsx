50d7f2de54211fa8d95222359c67bb2f880a80d5
mkreynolds919 <mkreynolds919@gmail.com>
2025-07-18 22:03:18 -0500

    feat: populate Header with proper textareas and give them updating data logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,28 @@
 
 export default function Header({ setData, header }) {
+
+    function handleHeaderData(e, item) {
+        const itemData = e.target.value;
+
+        setData(prevData => ({
+            ...prevData,
+            header: {
+                ...prevData.header,
+                [item]: itemData,
+            },
+        }));
+    }
+
     return (
         <>
+            <textarea className="name" onChange={e => handleHeaderData(e, "name")}>{header.name}</textarea>
+            <textarea className="title" onChange={e => handleHeaderData(e, "title")}>{header.title}</textarea>
+            <textarea className="location" onChange={e => handleHeaderData(e, "location")}>{header.location}</textarea>
+            <div className="contact-container">
+                <textarea className="phone" onChange={e => handleHeaderData(e, "phone")}>{header.phone}</textarea>
+                <span className="hypen">-</span>
+                <textarea className="email">{e => handleHeaderData(e, "email")}</textarea>
+            </div>
         </>
     );
 }
