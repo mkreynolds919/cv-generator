75a6e6888dc48b8bae046ae1ba1df9e6027c4430
mkreynolds919 <mkreynolds919@gmail.com>
2025-07-21 10:42:09 -0500

    feat: change Header textareas to inputs and add header-container div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,15 +14,15 @@ export default function Header({ data, setData }) {
     }
 
     return (
-        <>
-            <textarea className="name" onChange={e => handleHeaderData(e, "name")} value={data.name} />
-            <textarea className="title" onChange={e => handleHeaderData(e, "title")} value={data.title} />
-            <textarea className="location" onChange={e => handleHeaderData(e, "location")} value={data.location} />
+        <div className="header-container">
+            <input className="name" onChange={e => handleHeaderData(e, "name")} value={data.name} />
+            <input className="title" onChange={e => handleHeaderData(e, "title")} value={data.title} />
+            <input className="location" onChange={e => handleHeaderData(e, "location")} value={data.location} />
             <div className="contact-container">
-                <textarea className="phone" onChange={e => handleHeaderData(e, "phone")} value={data.phone} />
+                <input className="phone" onChange={e => handleHeaderData(e, "phone")} value={data.phone} />
                 <span className="hypen">-</span>
-                <textarea className="email" onChange={e => handleHeaderData(e, "email")} value={data.email} />
+                <input className="email" onChange={e => handleHeaderData(e, "email")} value={data.email} />
             </div>
-        </>
+        </div>
     );
 }
