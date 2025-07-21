5ca374a88bed3c17340f961e54b5275124e4a55b
mkreynolds919 <mkreynolds919@gmail.com>
2025-07-21 12:59:03 -0500

    fix: reorder input fields in Header component and adjust text alignment styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,9 +19,10 @@ export default function Header({ data, setData }) {
         <div className="header-container">
             <input className="name" onChange={e => handleHeaderData(e, "name")} value={data.name} />
             <input className="title" onChange={e => handleHeaderData(e, "title")} value={data.title} />
-            <input className="location" onChange={e => handleHeaderData(e, "location")} value={data.location} />
             <div className="contact-container">
-                <input className="phone" onChange={e => handleHeaderData(e, "phone")} value={data.phone} />
+                <input className="location" onChange={e => handleHeaderData(e, "location")} value={data.location} />
+                <span className="hypen">-</span>
+                <input className="phone" maxLength="12" onChange={e => handleHeaderData(e, "phone")} value={data.phone} />
                 <span className="hypen">-</span>
                 <input className="email" onChange={e => handleHeaderData(e, "email")} value={data.email} />
             </div>
