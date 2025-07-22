2e41f6ff6f8987428b1dab7463453bca13626ae3
mkreynolds919 <mkreynolds919@gmail.com>
2025-07-22 10:28:42 -0500

    feat: implement Summary component with state handling for summary input

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,7 +1,18 @@
 
 export default function Summary({ setData, summary }) {
+
+    function handleSummary(e) {
+        const newSummary = e.target.value;
+        setData((prevData) => ({
+            ...prevData,
+            summary: newSummary,
+        }));
+    }
     return (
-        <>
-        </>
+        <div className='summary-container'>
+            <h1 className='section-header'>Summary</h1>
+            <hr></hr>
+            <textarea className="Summary" value={summary} onChange={handleSummary}></textarea>
+        </div>
     );
 }
