6432702538d570666c148ad4112a1134d46f6259
mkreynolds919 <mkreynolds919@gmail.com>
2025-07-24 14:28:06 -0500

    feat: use established SectionInfo template to create Experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,25 @@
 
-export default function Experience({ setData, experience }) {
+import SectionInfo from "./SectionInfo.jsx";
+
+export default function Experience({ data, setData }) {
     return (
-        <>
-        </>
+            <div className="experience-container section">
+                <h1 className="section-header">Experience</h1>
+                <hr></hr>
+                {data.map(exp => {
+                    return <SectionInfo 
+                            name={exp.name} 
+                            location={exp.location} 
+                            startDate={exp.startDate}
+                            endDate={exp.endDate}
+                            subtitles={exp.subtitles}
+                            bullets={exp.bullets}
+                            setData={setData}
+                            key={exp.id}
+                            id={exp.id}
+                            source="experience"
+                            />
+                })}
+            </div>
     );
 }
