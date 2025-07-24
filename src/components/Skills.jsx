3e5a49759a67dbab5d32ff7a3ab4425e0b30a44a
mkreynolds919 <mkreynolds919@gmail.com>
2025-07-24 14:58:45 -0500

    feat: create Skills component with original structuring

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,30 @@
 
-export default function Skills({ setData, skills }) {
+export default function Skills({ data, setData }) {
+
+    function handleSkill(e, index) {
+        const itemData = e.target.value;
+
+        setData((prevData) => ({
+            ...prevData,
+            skills: prevData.skills.map((b, i) => i === index ? itemData : b),
+        }));
+    }
+
     return (
-        <>
-        </>
-    );
+            <div className="skills-container section">
+                <h1 className="section-header">Skills</h1>
+                <hr></hr>
+                <div className="inner">
+                    {data.map((skill, index) => {
+                        return (
+                            <>
+                                <input key={index} type="text" className="skill" value={skill} onChange={e => handleSkill(e, index)}></input>
+                                {index < data.length && <span className="hypen">-</span>}
+                            </>
+                        );
+                    })}
+                </div>
+                
+            </div>
+        );
 }
