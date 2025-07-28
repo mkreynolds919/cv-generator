7f125b25f517cea87762327952fe8abfe133dbab
mkreynolds919 <mkreynolds919@gmail.com>
2025-07-28 13:40:31 -0500

    feat: Add printing functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,27 @@
 import '../styles/App.css'
 import Page from "./Page.jsx";
 import initialData from "../data/initialData.js";
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function App() {
   const [data, setData] = useState(initialData);
   const [isEditing, setIsEditing] = useState(true);
   const { header, summary, education, experience, skills, projects } = data;
+  const printRef = useRef();
+
+  const handlePrint = () => {
+    window.print();
+  };
 
   return (
     <>
-      <Page setData={setData} header={header} summary={summary} education={education} experience={experience} skills={skills} projects={projects} isEditing={isEditing}/>
+      <div ref={printRef} className="cv-page">
+        <Page setData={setData} header={header} summary={summary} education={education} experience={experience} skills={skills} projects={projects} isEditing={isEditing}/>
+      </div>
       <div className="page-button-container">
         <button className="edit-button" onClick={() => setIsEditing(true)}>Edit</button>
         <button className="done-button" onClick={() => setIsEditing(false)}>Done</button>
+        <button className="print-button" onClick={() => handlePrint()}>Print</button>
       </div>
     </>
   )
