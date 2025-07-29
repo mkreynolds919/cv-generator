8cfaa4509f23a7f34ea00b662bcdef2f691e84b2
mkreynolds919 <mkreynolds919@gmail.com>
2025-07-29 12:34:47 -0500

    feat: install and implement color picker for name and section headers

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,14 +1,34 @@
-
+import { SketchPicker } from 'react-color';
 import "../styles/ControlPanel.css";
+import { useState } from 'react';
 
 export default function ControlPanel({ setIsEditing, handlePrint}) {
+    const [color, setColor] = useState("#000000");
+
+
+
+    function handleColor(clr) {
+        setColor(clr.hex);
+
+        const headers = document.querySelectorAll(".section-header");
+        const name = document.querySelector('.name');
+        headers.forEach((h) => {
+            h.style.color = clr.hex;
+        });
+        name.style.color = clr.hex;
+    }
+
+
     return (
         <div className="control-panel">
+            <div className="color-container">
+                <SketchPicker color={color} onChangeComplete={handleColor} />
+            </div>
             <div className="btn-container">
                 <button className="edit-button" onClick={() => setIsEditing(true)}>Edit</button>
                 <button className="done-button" onClick={() => setIsEditing(false)}>Done</button>
                 <button className="print-button" onClick={() => handlePrint()}>Print</button>
             </div>
         </div>
-    )
+    );
 }
